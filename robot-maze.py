40388e5d6da37924d31cd43a49289c4e7416ed3f
rogersheldon <rogersheldon@yahoo.com>
2019-10-30 13:34:37 -0400

    initial implementation

--- robot-maze.py
+++ robot-maze.py
@@ -1,2 +1,25 @@
 
-print 'hello world'
+print('hello world')
+
+print('done')
+
+# class Coordinate:
+
+# Returns an (angle, distance) tuple for navigating a robot
+# from fromCoordinate to toCoordinate with an initial direction of angle.
+def navigate(angle, fromCoordinate, toCoordinate):
+    print(fromCoordinate)
+    print(toCoordinate)
+    return(10.0, 20.0)
+
+def main():
+    coordinates = [ (0, 0), (25, 0), (38, 13) ]
+    angle = 0.0
+    for i in range(len(coordinates) - 1):
+        fromCoordinate = coordinates[i]
+        toCoordinate = coordinates[i + 1]
+        (angle, distance) = navigate(angle, fromCoordinate, toCoordinate)
+        print('Turn ', angle, 'degrees and move forward', distance, 'units')
+
+
+main()
